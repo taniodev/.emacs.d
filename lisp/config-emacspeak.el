ac3652c318a8dc24c197a32891f5255b2f4b5729
Tânio Scherer <tanioms3@gmail.com>
2019-12-10 11:36:31 -0300

    Define C-F4 para eliminar o buffer atual

--- lisp/config-emacspeak.el
+++ lisp/config-emacspeak.el
@@ -8,6 +8,11 @@
 ))
 
 
+;; Atalhos de teclado
+;; C-F4 para fechar o buffer atual sem perguntar
+(global-set-key (kbd "C-<f4>") 'emacspeak-kill-buffer-quietly)
+
+
 (load-file "/opt/emacspeak/lisp/emacspeak-setup.el")
 (provide 'config-emacspeak)
 
