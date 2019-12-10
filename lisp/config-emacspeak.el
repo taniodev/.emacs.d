c8cfea7010a2b61c38e3671a83b331e50dfe9079
Tânio Scherer <tanioms3@gmail.com>
2019-12-10 16:30:04 -0300

    Atribui atalhos para alternar entre buffers abertos
    * PAGEUP - Buffer anterior
    * PAGEDOWN - Próximo buffer

--- lisp/config-emacspeak.el
+++ lisp/config-emacspeak.el
@@ -12,6 +12,12 @@
 ;; C-F4 para fechar o buffer atual sem perguntar
 (global-set-key (kbd "C-<f4>") 'emacspeak-kill-buffer-quietly)
 
+;; C-PageUP para alternar ao buffer anterior que está aberto no mesmo modo
+(global-set-key (kbd "C-<prior>") 'emacspeak-wizards-cycle-to-previous-buffer)
+
+;; C-PageDown para alternar ao próximo buffer que está aberto no mesmo modo
+(global-set-key (kbd "C-<next>") 'emacspeak-wizards-cycle-to-next-buffer)
+
 
 (load-file "/opt/emacspeak/lisp/emacspeak-setup.el")
 (provide 'config-emacspeak)
