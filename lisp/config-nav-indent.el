028bf75ca62ab89ca20d8165a23e47a59e3c53c2
Tânio Scherer <tanioms3@gmail.com>
2021-03-18 19:02:45 -0300

    Cria uma função para navegar ao nível filho

--- lisp/config-nav-indent.el
+++ lisp/config-nav-indent.el
@@ -6,6 +6,9 @@
 ;; Control+Seta para cima - Navega para a linha anterior com o mesmo nível de indentação
 (global-set-key (kbd "C-<up>") 'nav-previous-block)
 
+;; Control+Seta para a direita - Navega para o próximo nível (Filho)
+(global-set-key (kbd "C-<right>") 'nav-next-level)
+
 
 (defun notify-with-emacspeak ()
   (emacspeak-auditory-icon 'large-movement)
@@ -38,6 +41,24 @@
     )   ;; Fim while
 
     (when (< (current-indentation) initial-indentation)
+      (goto-char point-start))
+  )   ;; Fim let
+  (notify-with-emacspeak)
+)
+
+(defun nav-next-level ()
+  "Navega para a linha do nível filho"
+  (interactive)
+  (let ((initial-indentation (current-indentation)) (point-start (point)))
+    (next-line)
+    (back-to-indentation)
+
+    (while (and (eolp) (not (eobp)))
+      (next-line)
+      (back-to-indentation)
+    )   ;; Fim while
+
+    (when (<= (current-indentation) initial-indentation)
       (goto-char point-start))
   )   ;; Fim let
   (notify-with-emacspeak)
