2a379d9051e6e241b176652595e0974adb220303
Tânio Scherer <tanioms3@gmail.com>
2021-03-20 17:56:59 -0300

    Cria algumas funções básicas para rodar testes

--- lisp/config-python.el
+++ lisp/config-python.el
@@ -4,6 +4,12 @@
 (add-hook 'python-mode-hook '(lambda ()
   ;; Ativar o ambiente virtual
   (define-key python-mode-map (kbd "C-S-a v") 'pyvenv-activate)
+
+  ;; Rodar todos os testes
+  (define-key python-mode-map (kbd "C-S-a r") 'python-rodar-testes)
+
+  ;; Rodar os testes do buffer atual
+  (define-key python-mode-map (kbd "C-S-a t") 'python-rodar-testes-buffer)
 ))
 
 
@@ -17,6 +23,23 @@
 (use-package pyvenv
   :ensure t
   :hook (python-mode . pyvenv-mode)
+)
+
+
+;; Funções para rodar testes
+(setq python-rodar-testes-cmd "pytest")
+(setq python-rodar-testes-args "-s")
+
+(defun python-rodar-testes ()
+  "Rodar todos os testes"
+  (interactive)
+  (compile (concat python-rodar-testes-cmd " " python-rodar-testes-args))
+)
+
+(defun python-rodar-testes-buffer ()
+  "Rodar todos os testes do buffer atual"
+  (interactive)
+  (compile (concat python-rodar-testes-cmd " " (buffer-file-name) " " python-rodar-testes-args))
 )
 
 (provide 'config-python)
