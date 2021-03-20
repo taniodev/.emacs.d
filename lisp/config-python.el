cad6df2261191dcbe16ce4417e5598ada92d6ff3
Tânio Scherer <tanioms3@gmail.com>
2021-03-20 15:16:53 -0300

    Adiciona e configura o pyvenv

--- lisp/config-python.el
+++ lisp/config-python.el
@@ -1,10 +1,22 @@
 ;; Configurações para desenvolvimento em Python
 
+;; Atalhos de tecla
+(add-hook 'python-mode-hook '(lambda ()
+  ;; Ativar o ambiente virtual
+  (define-key python-mode-map (kbd "C-S-a v") 'pyvenv-activate)
+))
+
+
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
     (require 'lsp-pyright)
     (lsp-deferred)))
+)
+
+(use-package pyvenv
+  :ensure t
+  :hook (python-mode . pyvenv-mode)
 )
 
 (provide 'config-python)
