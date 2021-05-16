b28c97edb9d8b28ef00baa54c4d57f342fe95932
Tânio Scherer <tanioms3@gmail.com>
2021-05-15 21:06:05 -0300

    Configura o prefixo Alt+L para o lsp-mode

--- lisp/config-lsp.el
+++ lisp/config-lsp.el
@@ -3,6 +3,8 @@
   :ensure t
   :commands (lsp lsp-deferred)
   :custom (lsp-disabled-clients '((python-mode . pyls)))
+  :init
+  (setq lsp-keymap-prefix "M-l")
   :config
   (setq lsp-signature-auto-activate nil)
   :hook (python-mode . lsp-deferred)
