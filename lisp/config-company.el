6a6655409c4a0608f042802ef9e9044c87155b37
Tânio Scherer <tanioms3@gmail.com>
2021-06-03 15:32:40 -0300

    Apresenta resultados do yasnippet no company

--- lisp/config-company.el
+++ lisp/config-company.el
@@ -5,9 +5,18 @@
   :config
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 3)
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
   ;; Control+TAB - Chamar o preenchimento manualmente
   :bind ("C-<tab>" . company-complete)
 )
+
+
+;; Habilita o suporte do yasnippet para todos os backends do company
+(defun company-mode/backend-with-yas (backend)
+  (if (and (listp backend) (member 'company-yasnippet backend))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
 
 (provide 'config-company)
