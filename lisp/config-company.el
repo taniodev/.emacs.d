d95f2b34a28220c543ed6d6c2823d88a3cfce10a
Tânio Scherer <tanioms3@gmail.com>
2022-01-31 11:40:41 -0300

    Configuração do Company ao estilo do use-package

--- lisp/config-company.el
+++ lisp/config-company.el
@@ -1,22 +1,13 @@
-;; Configurações do módulo de preenchimento Company (para obter auto complete)
+;; Configurações do módulo de preenchimento Company (para obter autocomplete)
 
 (use-package company
   :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 3)
-  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  :custom
+  (company-idle-delay 0)
+  (company-minimum-prefix-length 3)
 
   ;; Control+TAB - Chamar o preenchimento manualmente
   :bind ("C-<tab>" . company-complete)
 )
-
-
-;; Habilita o suporte do yasnippet para todos os backends do company
-(defun company-mode/backend-with-yas (backend)
-  (if (and (listp backend) (member 'company-yasnippet backend))
-      backend
-    (append (if (consp backend) backend (list backend))
-            '(:with company-yasnippet))))
 
 (provide 'config-company)
