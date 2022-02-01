1890c00902a55f784da0f7e95d1539c1f57736ea
Tânio Scherer <tanioms3@gmail.com>
2022-02-01 15:36:16 -0300

    Configurações do Magit ao estilo do use-package

--- lisp/config-magit.el
+++ lisp/config-magit.el
@@ -2,18 +2,16 @@
 
 (use-package magit
   :ensure t
-  :config
+  :custom
   ;; Tamanho máximo da linha de resumo do commit
-  (setq git-commit-summary-max-length 50)
+  (git-commit-summary-max-length 50)
   ;; Coluna a partir da qual deve fazer a quebra de linha automaticamente
-  (setq git-commit-fill-column 72)
+  (git-commit-fill-column 72)
   ;; Habilita as verificações do texto antes de fazer o commit
-  (setq git-commit-style-convention-checks '(non-empty-second-line overlong-summary-line))
+  (git-commit-style-convention-checks '(non-empty-second-line overlong-summary-line))
 
   ;; Ctrl+Shift+g - Abrir o buffer do magit-status
   :bind ("C-S-g" . magit-status)
 )
 
-
 (provide 'config-magit)
-
