1fbbe181aebebe6fa9fb965fb4a22ef5c2843e63
Tânio Scherer <tanioms3@gmail.com>
2022-02-18 15:19:46 -0300

    Fecha tags html automaticamente
    As configurações dos tipos de arquivo que habilitam o web-mode foram
    movidas para a sessão :init no use-package. Anteriormente ele não estava
    sendo carregado corretamente.
    
    O hook que era usado para configurar algumas variáveis foi removido, e
    essas definições movidas para a sessão :custom do use-package.

--- lisp/config-web.el
+++ lisp/config-web.el
@@ -1,19 +1,18 @@
-
-(defun meu-web-mode-hook ()
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-)
+;; Tecnologias web: html, css, js, etc.
 
 (use-package web-mode
   :ensure t
-  :config
+  :custom
+  (web-mode-auto-close-style 2)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+
+  :init
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
-  :hook
-  (web-mode . meu-web-mode-hook)
 )
 
 (provide 'config-web)
