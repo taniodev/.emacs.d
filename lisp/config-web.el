396d8bf2e5361b6562e4ea7d669437402224613d
Tânio Scherer <tanioms3@gmail.com>
2022-11-01 16:17:47 -0300

    web-mode: Ajusta a indentação na esquerda da linha

--- lisp/config-web.el
+++ lisp/config-web.el
@@ -1,12 +1,12 @@
-;; Tecnologias web: html, css, js, etc.
+;; Edição de HTML e CSS
 
 (use-package web-mode
   :ensure t
   :custom
   (web-mode-auto-close-style 2)
-  (web-mode-markup-indent-offset 2)
-  (web-mode-css-indent-offset 2)
-  (web-mode-code-indent-offset 2)
+  ;; Indentação no lado esquerdo da linha
+  (web-mode-style-padding 4)   ;; Para tag <style>
+  (web-mode-script-padding 4)   ;; Para tag <script>
 
   :init
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
