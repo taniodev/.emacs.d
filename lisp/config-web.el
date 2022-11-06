7f61831a406ea3ac22a006f451de04d729ab0bf7
Tânio Scherer <tanioms3@gmail.com>
2022-11-05 22:36:17 -0300

    Não abra arquivos JavaScript usando o web-mode

--- lisp/config-web.el
+++ lisp/config-web.el
@@ -11,7 +11,6 @@
   :init
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
 )
 
